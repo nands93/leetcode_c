4fc153f837833befdb2a1d2f5760df33fed112b9
nands93 <femmarques93+github@gmail.com>
2025-05-09 17:24:44 -0300

    refactor longestCommonPrefix function for improved readability and correctness

--- 14_longest-common-prefix.c
+++ 14_longest-common-prefix.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0 || strs[0] == NULL)
+        return "";
     int i = 0;
-    while (strs[i])
+    int len = strlen(strs[0]);
+    char *result = (char*)malloc(len + 1);
+    result = strcpy(result, strs[0]);
+    for (int i = 0; i <= strsSize - 1; i++)
     {
+        int j = 0;
+        while (result[j] && strs[i][j] && result[j] == strs[i][j])
+            j++;
+        result[j] = '\0';
     }
-    
+    return (result);
 }
 
 int main()
